1266c0b09d61d32b7f0269c0ef009d92f6fe096b
ilya <ponoia@yandex.ru>
2024-10-11 16:04:46 +0300

    parsing_update

--- parsing.py
+++ parsing.py
@@ -2,20 +2,16 @@ from xml.dom.minidom import *
 
 DATA_PATH = 'currency.xml'
 
-
-def get_title(data):
-    data.seek(0)
-    title = next(data)
-    title = title.split(';')
-    title = [el.strip() for el in title]
-    return title
-
-
-def get_object(line, title):
-    pass
-
+def diction_maker():
+    dictionary = {}
+    charcodes = document.getElementsByTagName('CharCode')
+    names = document.getElementsByTagName('Name')
+    for i in range(len(charcodes)):
+        name = names[i]
+        charcode = charcodes[i]
+        dictionary[name.firstChild.data] = charcode.firstChild.data
+    return dictionary
 
 if __name__ == '__main__':
-    with parse(DATA_PATH, 'r') as file:
-        data = file.read()
-        print(data)
+    with parse(DATA_PATH) as document:
+        print(diction_maker())
